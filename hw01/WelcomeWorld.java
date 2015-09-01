f5a1cb2f976d7cc43f5720ae521e11742d4ebf20
dvk219 <dvk219@lehigh.edu>
2015-09-01 02:25:21 +0000

    updated WelcomeWorld

---
+++ hw01/WelcomeWorld.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////
+//Dmitriy Kim
+//Homework 1
+//WelcomeWorld Java Program
+//
+//  first complete the program
+//      javac WelcomeWorld.java
+//  run the program
+//      java WelcomeWorld//
+
+//define a class
+public class  WelcomeWorld{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the statement with box around it
+    System.out.println("     -----------");
+    System.out.println("     | WELCOME |");
+    System.out.println("     -----------");
+    System.out.println("   ^    ^    ^    ^    ");
+    System.out.println(" // \\// \\// \\// \\ ");
+    System.out.println("|-D--V--K--2--1--9-|  ");
+    System.out.println(" \\ //\\ //\\ // \\ // ");
+    System.out.println("   v    v    v     v    ");
+    System.out.println("This August me and my family visited lake Issyk-Kul"); 
+    System.out.println("Kyrgyzstan. At night, we could see stars and Milky Way");
+    System.out.println("and I took photos of it. It was beautiful.");
+
+//test/testtest
+    }
+}
