409ebca1dbfece3036d6b6f2e6852009f62f3cfb
dvk219 <dvk219@lehigh.edu>
2015-09-22 21:02:10 +0000

    updated Timer

--- hw03/Timer.java
+++ hw03/Timer.java
@@ -6,15 +6,15 @@
 import java.util.Scanner; //importing scanner
 
 public class Timer{ //creating class
-public static void main(String[] args)
+public static void main(String[] args) //setting the maing method
 {
-    Scanner myscanner=new Scanner(System.in);
+    Scanner myscanner=new Scanner(System.in); // creating scanner
     System.out.println("Enter the current time in military style: xx(h).yy(min)");
-    int a = myscanner.nextInt();
+    int a = myscanner.nextInt();//settign variable for a
     System.out.println("Enter the time of dinner");
-    int b = myscanner.nextInt();
-    int h=(b/100)-(a/100);
-    int m=(b % 100)-(a % 100);    
-    System.out.println("The remaning time is " +h+ ":" +m);
+    int b = myscanner.nextInt(); // setting variable for b
+    int h=(b/100)-(a/100); //converting hours to 2-digit and finding the amount of hours
+    int m=(b % 100)-(a % 100); // finding the remainder =minutes   
+    System.out.println("The remaning time is " +h+ ":" +m); // creating the output adding hours and minutes seperately
 }
 }
